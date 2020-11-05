37d6f0cafa0c97c5fefcf588fbc208f9a8643d74
KuRoSun0 <45484469+KuRoSun0@users.noreply.github.com>
2020-11-06 01:49:10 +0700

    ระบบประตู
    กดปุ่มแล้วประตูจะเปิด

---
+++ Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool isPlayer,isBox;
+    public Transform buttonCheack;
+    public LayerMask whatIsPlayer,whatIsBox;
+    public float checkRadius,speed,doorPositionY,doorMove;
+    private void Start()
+    {
+        isPlayer = false;
+        doorPositionY = transform.position.y;
+    }
+    void FixedUpdate()
+    {
+            isPlayer = Physics2D.OverlapCircle(buttonCheack.position, checkRadius, whatIsPlayer);
+            isBox = Physics2D.OverlapCircle(buttonCheack.position, checkRadius, whatIsBox);
+    }
+    void Update()
+    {
+        if(isPlayer == true || isBox == true)
+        {
+            if (transform.position.y < doorPositionY + doorMove)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+            }
+        }
+        else
+        {
+            if (transform.position.y>=doorPositionY) 
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+            }
+        }
+    }
+}
