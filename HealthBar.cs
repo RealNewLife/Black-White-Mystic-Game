74b4a11efb0d557a7209914b7a03ddb5bb5a6588
KuRoSun0 <45484469+KuRoSun0@users.noreply.github.com>
2020-11-06 01:47:43 +0700

    หลอดเลือด
    ระบบแสดงหลอดเลือด

---
+++ HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    
+    public void SetHealthMax(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(int heal)
+    {
+        slider.value = heal;
+    }
+}
