08c1fcf5d6cd559f9e1d7c67e1840dd123493f56
KuRoSun0 <45484469+KuRoSun0@users.noreply.github.com>
2021-01-21 20:26:42 +0700

    ระบบเปลี่ยนสี

---
+++ ChangingColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingColor : MonoBehaviour
+{
+
+    public Renderer myObject;
+    public Material material1;
+    public Material material2;
+    public float cx=0;
+    public float cy = 0;
+
+    public void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+      if(other.gameObject.tag == "bullet1")
+        {
+            transform.position = new Vector3(transform.position.x+cx, transform.position.y+cy, 0);
+            myObject.sharedMaterial = material1;
+        }
+        if (other.gameObject.tag == "bullet2")
+        {
+            transform.position = new Vector3(transform.position.x-cx, transform.position.y-cy, -1);
+            myObject.sharedMaterial = material2;
+        }
+    }
+}
