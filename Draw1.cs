473eed260359dcbf2d99200d389088bfe7f21a97
KuRoSun0 <45484469+KuRoSun0@users.noreply.github.com>
2021-01-21 20:29:40 +0700

    ระบบวาด
    เป็นระบบวาดหรือแสดงดูที่ต้องการที่จะบอกเพื่อน

---
+++ Draw1.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw1 : MonoBehaviour
+{
+    public Camera m_camera;
+    public GameObject brush;
+    public float posDraw;
+    LineRenderer currentLineRenderer;
+
+    Vector3 lastPos;
+    bool activeSkill = true;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            activeSkill = true;
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            activeSkill = false;
+        }
+        if(activeSkill == true)
+        {
+            Drawing();
+        }
+    }
+    void Drawing()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CreateBrush();
+        }
+        else if (Input.GetKey(KeyCode.Mouse0))
+        {
+            PointToMousePos();
+        }
+        else
+        {
+            currentLineRenderer = null;
+        }
+    }
+
+    void CreateBrush()
+    {
+        GameObject brushInstance = Instantiate(brush);
+        currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
+        
+        //because you gotta have 2 points to start a line renderer, 
+        Vector3 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = posDraw;
+        currentLineRenderer.SetPosition(0, mousePos);
+        currentLineRenderer.SetPosition(1, mousePos);
+
+    }
+
+    void AddAPoint(Vector3 pointPos)
+    {
+        currentLineRenderer.positionCount++;
+        int positionIndex = currentLineRenderer.positionCount - 1;
+        pointPos.z = posDraw;
+        currentLineRenderer.SetPosition(positionIndex, pointPos);
+    }
+
+    void PointToMousePos()
+    {
+        Vector3 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        if (lastPos != mousePos)
+        {
+            AddAPoint(mousePos);
+            lastPos = mousePos;
+        }
+    }
+
+}
