fc0e4a27613ca7b2b83e70a26a2df2c5487f710e
KuRoSun0 <45484469+KuRoSun0@users.noreply.github.com>
2021-02-19 20:11:08 +0700

    Add files via upload

---
+++ Button3.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Button3 : MonoBehaviour
+{
+    //public bool x = false;
+    //public Puzzle1 puzzle1;
+    //private void Awake()
+    //{
+    //    puzzle1 = FindObjectOfType<Puzzle1>();
+    //}
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "bullet1" || other.gameObject.tag == "bullet2")
+    //    {
+    //        x = true;
+    //    }
+    //}
+
+    //public void SetX()
+    //{
+    //    x = puzzle1.x3;
+    //}
+
+
+    private PhotonView photonView;
+    public bool x = false;
+    public Puzzle1 puzzle1;
+    private void Awake()
+    {
+        photonView = PhotonView.Get(this);
+        puzzle1 = FindObjectOfType<Puzzle1>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "bullet1" || other.gameObject.tag == "bullet2")
+        {
+            photonView.RPC("valueX3", RpcTarget.AllBuffered);
+        }
+    }
+
+    public void SetX()
+    {
+        photonView.RPC("SetX3", RpcTarget.AllBuffered);
+    }
+    [PunRPC]
+    void valueX3()
+    {
+        x = true;
+    }
+    [PunRPC]
+    void SetX3()
+    {
+        x = puzzle1.x3;
+    }
+}
---
+++ PlatForm1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlatForm1 : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+    public Transform platForm1;
+    public float speed;
+    private bool x = false; 
+    Vector3 nextPos;
+    PhotonView photonView;
+
+    void Awake()
+    {
+        photonView = PhotonView.Get(this);
+    }
+    private void Update()
+    {
+        if (x == true)
+        {
+            platForm1.transform.position = Vector3.MoveTowards(platForm1.transform.position, pos1.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            platForm1.transform.position = Vector3.MoveTowards(platForm1.transform.position, pos2.position, speed * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player1"|| other.gameObject.tag == "Player2")
+        {
+            x = true;
+            //photonView.RPC("ButtonOn1", RpcTarget.AllBuffered);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+            x = false;
+            //photonView.RPC("ButtonOn2", RpcTarget.AllBuffered);
+
+        
+            
+    }
+    [PunRPC]
+    void ButtonOn1()
+    {
+       //platForm1.transform.position = Vector3.MoveTowards(transform.position, pos1.position, speed * Time.deltaTime);
+    }
+    [PunRPC]
+    void ButtonOn2()
+    {
+       //platForm1.transform.position = Vector3.MoveTowards(transform.position,pos2.position, speed * Time.deltaTime);
+    }
+}
